1791d0c158f489f21d4dfc5ee9f09d3fe0f38a75
emmeiwhite <emmeiwhite@gmail.com>
2020-04-11 10:22:15 +0530

    We have developed the game board thus far

--- script.js
+++ script.js
@@ -1,3 +1,76 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  // code goes here
+  // all js code goes here
+
+  /* --- 1. Let's create our cards array which we'll be using in our game --- */
+
+  const cardArray = [
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "ice-cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "ice-cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+  ];
+
+  // let's create our card borad first
+
+  const grid = document.querySelector(".grid");
+
+  const createboard = () => {
+    // adding blank.png image everywhere on our board. For that we'll create an img element
+    console.log(cardArray.length);
+    cardArray.forEach((card, index) => {
+      var card = document.createElement("img");
+      card.setAttribute("src", "./images/blank.png");
+      card.setAttribute("data-id", index);
+
+      card.addEventListener("click", flipcard);
+
+      grid.appendChild(card);
+    });
+  };
+
+  createboard();
 });
